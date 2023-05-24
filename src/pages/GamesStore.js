2be63bcab6829efbe42b47ad2bd6fd14a7f0ab28
SaranKaewchuay <saranyeen@gmail.com>
2023-05-24 15:18:58 +0700

    update

--- src/pages/GamesStore.js
+++ src/pages/GamesStore.js
@@ -98,7 +98,7 @@ class GamesStore extends React.Component {
   };
   render() {
     return (
-      <div class="container-xl fixed "  style={{ color: '#0a4275', marginTop:"110px"}}>
+      <div class="container-xxl fixed "  style={{ color: '#0a4275', marginTop:"110px"}}>
         <div>
           <div className="page-content">
             <main className="page-main">
